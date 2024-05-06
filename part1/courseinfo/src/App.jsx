eb21cee69abf32bafead0b6d136e536f1c68809a
Naziru Abdussalam Ibrahim <naziruabdussalami@gmail.com>
2024-05-06 10:04:48 +0100

    Add ex1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -9,21 +9,24 @@ const Header = (props) => {
   )
 }
 
-
+// Part component 
+const Part = ({part, exercise})=>{
+  return(
+    <>
+      <p>
+        {part} {exercise}
+      </p>
+    </>
+  )
+}
 
 // Content component 
 const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
   return(
     <>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part part={part1} exercise={exercises1} />
+      <Part part={part2} exercise={exercises2} />
+      <Part part={part3} exercise={exercises3} />
     </>
   )
 }
@@ -39,7 +42,7 @@ const Total = (props)=>{
   )
 }
 
-// the root component App
+// The root App component 
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -56,7 +59,6 @@ const App = () => {
         part1={part1} exercises1={exercises1} 
         part2={part2} exercises2={exercises2} 
         part3={part3} exercises3={exercises3} 
-      
       />
 
       <Total exercises={exercises1 + exercises2 + exercises3}/>
